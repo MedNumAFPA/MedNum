0cfb335a9bb8279c6863b00cdb4421afa189b207
amineshwarz <elkhal.medamine@gmail.com>
2025-08-27 11:30:25 +0200

    CRUD User pour admin

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,13 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -26,19 +27,22 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [ // oblige l’utilisateur à taper le mot de passe deux fois (pour éviter les erreurs de saisie).
+
+                'type' => PasswordType::class,
+                'invalid_message' => 'ressaye a nouveaux ça ne correspond pas.', // message affiché si les deux champs ne correspondent pas.
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password'], // dit au navigateur qu’il s’agit d’un nouveau mot de passe (meilleure gestion de la sécurité).
+                'required' => true,
+                'first_options' => ['label' => 'mot de passe'],
+                'second_options' => ['label' => 'confirmer mot de passe'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Please enter a password', // le mot de passe est obligatoire.
                     ]),
                     new Length([
-                        'min' => 6,
+                        'min' => 6, // minimum de 6 caractères, maximum de 4096 (limite de sécurité de Symfony).
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
---
+++ src/Form/UserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            // ->add('roles')
+            ->add('password')
+        ;
+        if ($options['allow_roles_update'] ?? false){
+            $builder->add('roles', ChoiceType::class, [
+                'label' => 'Rôles Utilisateur',
+                'choices' => [  
+                    'Administrateur' => 'ROLE_ADMIN', 
+                    'Editeur' => 'ROLE_EDITOR',
+                    'Utilisateur' => 'ROLE_USER',
+                    // Autres rôles...
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ]);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'allow_roles_update' => false, // Par défaut, l'édition des rôles est désactivée
+
+        ]);
+    }
+}
